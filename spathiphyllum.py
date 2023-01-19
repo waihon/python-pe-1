881835365b6e4be5890429c215368b95023d7551
Waihon Yew <yewwaihon@gmail.com>
2023-01-20 00:05:53 +0800

    Create spathiphyllum.py

---
+++ spathiphyllum.py
@@ -0,0 +1,8 @@
+plant = input("Enter the best plant ever: ")
+
+if plant == "Spathiphyllum":
+    print("Yes - Spathiphyllum is the best plant ever!")
+elif plant == "spathiphyllum":
+    print("No, I want a big Spathiphyllum!")
+else:
+    print(f"Spathiphyllum! Not {plant}!")
