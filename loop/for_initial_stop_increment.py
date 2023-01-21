fc74e6de3af33235eb48a363253d1584153e4da3
Waihon Yew <yewwaihon@gmail.com>
2023-01-21 18:34:36 +0800

    Create for_initial_stop_increment.py

---
+++ loop/for_initial_stop_increment.py
@@ -0,0 +1,9 @@
+# The 2nd argument shows the first value the control variable
+# will not be assigned.
+for i in range(2, 8, 3):
+    print("The value of i is currently", i)
+
+"""
+The value of i is currently 2
+The value of i is currently 5
+"""
