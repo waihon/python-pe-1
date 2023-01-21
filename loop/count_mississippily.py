cf7d693cf000a9fc703515a997625917f24953b7
Waihon Yew <yewwaihon@gmail.com>
2023-01-21 18:42:27 +0800

    Create count_mississippily.py

---
+++ loop/count_mississippily.py
@@ -0,0 +1,16 @@
+import time
+
+for i in range(1, 6):
+    print(i, "Mississippi")
+    time.sleep(1)
+
+print("Ready or not, here I come!")
+
+"""
+1 Mississippi
+2 Mississippi
+3 Mississippi
+4 Mississippi
+5 Mississippi
+Ready or not, here I come!
+"""
