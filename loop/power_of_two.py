ef60a732e42c18e1fdd54f3a76621a7cbc086b30
Waihon Yew <yewwaihon@gmail.com>
2023-01-21 18:36:06 +0800

    Create power_of_two.py

---
+++ loop/power_of_two.py
@@ -0,0 +1,23 @@
+power = 1
+for expo in range(16):
+    print("2 to the poser of", expo, "is", power)
+    power *= 2
+
+"""
+2 to the poser of 0 is 1
+2 to the poser of 1 is 2
+2 to the poser of 2 is 4
+2 to the poser of 3 is 8
+2 to the poser of 4 is 16
+2 to the poser of 5 is 32
+2 to the poser of 6 is 64
+2 to the poser of 7 is 128
+2 to the poser of 8 is 256
+2 to the poser of 9 is 512
+2 to the poser of 10 is 1024
+2 to the poser of 11 is 2048
+2 to the poser of 12 is 4096
+2 to the poser of 13 is 8192
+2 to the poser of 14 is 16384
+2 to the poser of 15 is 32768
+"""
