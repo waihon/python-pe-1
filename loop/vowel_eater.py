c0192df89445c5aa29a3cc7ac594ab03d99b9e73
Waihon Yew <yewwaihon@gmail.com>
2023-01-22 16:08:23 +0800

    Update and rename continue_vowels.py to vowel_eater.py

--- loop/vowel_eater.py
+++ loop/vowel_eater.py
@@ -1,3 +1,5 @@
+word_without_vowels = ""
+
 # Prompt the user to enter a word
 # and assign it to the user_word variable.
 user_word = input("Enter a word: ")
@@ -7,4 +9,7 @@ for letter in user_word:
     # Complete the body of the for loop.
     if letter in "AEIOU":
         continue
-    print(letter)
+    word_without_vowels += letter
+
+# Print the word assigned to word_without_vowels.
+print(word_without_vowels)
