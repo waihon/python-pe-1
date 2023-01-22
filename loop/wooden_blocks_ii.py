fde8873a5ccc0c7b6b148b7690de783f7e04c544
Waihon Yew <yewwaihon@gmail.com>
2023-01-22 17:35:56 +0800

    Create wooden_blocks_ii.py

---
+++ loop/wooden_blocks_ii.py
@@ -0,0 +1,30 @@
+blocks = int(input("Enter the number of blocks: "))
+
+height = 0
+used_blocks = 0
+remaining_blocks = 0
+
+if blocks >= 1:
+    height = 1
+    remaining_blocks = blocks - height
+
+while (height + 1) <= remaining_blocks:
+    height += 1
+    remaining_blocks -= height
+
+print("The height of the pyramid:", height)
+
+"""
+Enter the number of blocks: 6
+The height of the pyramid: 3
+Enter the number of blocks: 20
+The height of the pyramid: 5
+Enter the number of blocks: 1000
+The height of the pyramid: 44
+Enter the number of blocks: 2
+The height of the pyramid: 1
+Enter the number of blocks: 1
+The height of the pyramid: 1
+Enter the number of blocks: 0
+The height of the pyramid: 0
+"""
