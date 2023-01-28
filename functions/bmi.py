46213af3c837c97cc3e7cd399de2c8420ff5e104
Waihon Yew <yewwaihon@gmail.com>
2023-01-28 09:32:04 +0800

    Update bmi.py

--- functions/bmi.py
+++ functions/bmi.py
@@ -1,10 +1,12 @@
 def bmi(weight, height):
-    if height < 1.0 or height > 2.5 or \
-    weight < 20 or weight > 200:
+    # Valid weight is between infant's and that of heaviest man ever.
+    # Valid height is between infant's and that of tallest man ever.
+    if weight < 3.0 or weight > 640.0 or \
+    height < 0.45 or height > 2.8:
         return None
 
     return weight / height ** 2
 
 if __name__ == "__main__":
     print(bmi(70.0, 1.58))  # 28.040378144528116
-    print(bmi(352.5, 1.65)) # None
+    print(bmi(641.0, 1.65)) # None
