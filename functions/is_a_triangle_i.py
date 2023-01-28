b64d59f1132986e032385fef295709df2a973bcb
Waihon Yew <yewwaihon@gmail.com>
2023-01-28 10:24:00 +0800

    Create is_a_triangle_i.py

---
+++ functions/is_a_triangle_i.py
@@ -0,0 +1,20 @@
+def is_a_triangle(a, b, c):
+    # To form a triangle, the sum of two arbitrary sides
+    # (a and b), has to be longer than the third side (c),
+    # where (a - b) < c < (a + b).
+    if a + b <= c:
+        return False
+    if b + c <= a:
+        return False
+    if c + a <= b:
+        return False
+    return True
+
+if __name__ == "__main__":
+    print(is_a_triangle(1, 1, 1)) # True
+    print(is_a_triangle(3, 4, 5)) # True
+    print(is_a_triangle(4, 5, 3)) # True
+    print(is_a_triangle(5, 3, 4)) # True
+    print(is_a_triangle(1, 1, 3)) # False
+    print(is_a_triangle(1, 3, 1)) # False
+    print(is_a_triangle(3, 1, 1)) # False
