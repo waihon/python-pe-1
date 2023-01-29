4b259a628bfc6156166827cc420b1d9656a03af5
Waihon Yew <yewwaihon@gmail.com>
2023-01-29 09:31:04 +0800

    Create factorial.py

---
+++ functions/factorial.py
@@ -0,0 +1,24 @@
+def factorial(n):
+    if n < 0:
+        return None
+    if n < 2:
+        return 1
+
+    product = 1
+    for i in range(2, n+1):
+        product *= i
+
+    return product
+
+if __name__ == "__main__":
+    for n in range(-1, 6):
+        print(n, factorial(n))
+    """
+    -1 0
+    0 1
+    1 1
+    2 2
+    3 6
+    4 24
+    5 120
+    """
