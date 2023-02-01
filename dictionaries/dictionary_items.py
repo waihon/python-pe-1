0e45169f7f42822ee17162ebb5aa847ff4029b21
Waihon Yew <yewwaihon@gmail.com>
2023-02-01 15:50:55 +0800

    Create dictionary_items.py

---
+++ dictionaries/dictionary_items.py
@@ -0,0 +1,12 @@
+en_fr_animals = {"dog": "chien",
+                 "cat": "chat",
+                 "horse": "cheval"}
+
+for english, french in en_fr_animals.items():
+    print(english, "->", french)
+
+"""
+dog -> chien
+cat -> chat
+horse -> cheval
+"""
