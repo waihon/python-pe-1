4b5986be1bde3dc017d82fd1c16b58a010573a06
Waihon Yew <yewwaihon@gmail.com>
2023-02-01 20:29:10 +0800

    Create tuple_to_dictionary_i.py

---
+++ tuples/tuple_to_dictionary_i.py
@@ -0,0 +1,10 @@
+colors = (("green", "#00800"), ("blue", "#0000FF"))
+
+colors_dict = {}
+for color, code in colors:
+    colors_dict[color] = code
+
+print(colors_dict)
+"""
+{'green': '#00800', 'blue': '#0000FF'}
+"""
