ba92a691c2360ea3b75bdf368857e378168a0147
Waihon Yew <yewwaihon@gmail.com>
2023-02-01 18:57:49 +0800

    Create school_class.py

---
+++ dictionaries/school_class.py
@@ -0,0 +1,42 @@
+school_class = {}
+
+while True:
+    name = input("Enter the student's name: ")
+    if name == '':
+        break
+    
+    score = int(input("Enter the student's score (0-10): "))
+    if score not in range(0, 11):
+	    break
+    
+    if name in school_class:
+        school_class[name] += (score,)
+    else:
+        school_class[name] = (score,)
+        
+for name in sorted(school_class.keys()):
+    adding = 0
+    counter = 0
+    for score in school_class[name]:
+        adding += score
+        counter += 1
+    print(name, ":", adding / counter)
+
+"""
+Enter the student's name: Joe
+Enter the student's score (0-10): 7
+Enter the student's name: Mary
+Enter the student's score (0-10): 8
+Enter the student's name: Peter
+Enter the student's score (0-10): 6
+Enter the student's name: Joe
+Enter the student's score (0-10): 5
+Enter the student's name: Mary
+Enter the student's score (0-10): 9
+Enter the student's name: Peter
+Enter the student's score (0-10): 4
+Enter the student's name:
+Joe : 6.0
+Mary : 8.5
+Peter : 5.0
+"""
