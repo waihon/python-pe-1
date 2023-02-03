fcd31b4daca68344a042b8442f63e5ab63963c04
Waihon Yew <yewwaihon@gmail.com>
2023-02-03 08:32:54 +0800

    Update reciprocal.py

--- exceptions/reciprocal.py
+++ exceptions/reciprocal.py
@@ -1,6 +1,16 @@
 try:
     value = int(input('Enter a natural number: '))
     print('The reciprocal of', value, 'is', 1/value)        
+except ValueError:
+    print('I do not know what to do.')    
+except ZeroDivisionError:
+    print('Division by zero is not allowed in our Universe.') 
 
-except:
-    print('I do not know what to do.')
+"""
+Enter a natural number: 4
+The reciprocal of 4 is 0.25
+Enter a natural number: four
+I do not know what to do.
+Enter a natural number: 0
+Division by zero is not allowed in our Universe.
+"""
