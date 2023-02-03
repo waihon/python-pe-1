35c99b3e1d502bbaf9093c9046d7efaed228d951
Waihon Yew <yewwaihon@gmail.com>
2023-02-03 08:39:08 +0800

    Add a default branch

--- exceptions/reciprocal.py
+++ exceptions/reciprocal.py
@@ -1,10 +1,12 @@
 try:
     value = int(input('Enter a natural number: '))
-    print('The reciprocal of', value, 'is', 1/value)        
+    print('The reciprocal of', value, 'is', 1/value)
 except ValueError:
-    print('I do not know what to do.')    
+    print('I do not know what to do.')
 except ZeroDivisionError:
-    print('Division by zero is not allowed in our Universe.') 
+    print('Division by zero is not allowed in our Universe.')
+except:
+    print('Something strange has happened here... Sorry!')
 
 """
 Enter a natural number: 4
