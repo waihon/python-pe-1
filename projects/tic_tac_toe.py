5f7bc3d18caab0ae3dd1ea45450bfe2990e41a71
Waihon Yew <yewwaihon@gmail.com>
2023-02-05 08:44:20 +0800

    Do nothing for the time being

--- projects/tic_tac_toe.py
+++ projects/tic_tac_toe.py
@@ -150,24 +150,29 @@ o implement the functions defined for you in the editor.
 def display_board(board):
     # The function accepts one parameter containing the board's current status
     # and prints it out to the console.
+    pass
 
 
 def enter_move(board):
     # The function accepts the board's current status, asks the user about their
     # move, checks the input, and updates the board according to the user's
     # decision.
+    pass
 
 
 def make_list_of_free_fields(board):
     # The function browses the board and builds a list of all the free squares; 
     # the list consists of tuples, while each tuple is a pair of row and column
     # numbers.
+    pass
 
 
 def victory_for(board, sign):
     # The function analyzes the board's status in order to check if the player
     # using 'O's or 'X's has won the game.
+    pass
 
 
 def draw_move(board):
     # The function draws the computer's move and updates the board.
+    pass
