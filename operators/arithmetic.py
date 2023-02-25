0563c4d27a84f5ec6743705f15f6c3a8109a71f2
Waihon Yew <yewwaihon@gmail.com>
2023-02-25 11:34:42 +0800

    Create arithmetic.py

---
+++ operators/arithmetic.py
@@ -0,0 +1,15 @@
+# input a float value for variable a here
+a = float(input("Enter a number: "))
+# input a float value for variable b here
+b = float(input("Enter another number: "))
+
+# output the result of addition here
+print(a, "+", b, "=", a + b)
+# output the result of subtraction here
+print(a, "-", b, "=", a - b)
+# output the result of multiplication here
+print(a, "*", b, "=", a * b)
+# output the result of division here
+print(a, "/", b, "=", a / b)
+
+print("\nThat's all, folks!")
