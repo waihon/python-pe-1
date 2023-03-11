30a18818032f4b26a49136af5b7651f6861d07e9
Waihon Yew <yewwaihon@gmail.com>
2023-03-11 12:56:53 +0800

    Update is_prime.py

--- functions/is_prime.py
+++ functions/is_prime.py
@@ -3,14 +3,15 @@ def is_prime(num):
         return False
     elif num == 2:
         return True
-    
+
     max_divisor = num // 2 + 1
     for div in range(2, max_divisor + 1):
         if num % div == 0:
             return False
     return True
 
-for i in range(1, 20):
-	if is_prime(i + 1):
-			print(i + 1, end=" ")
-print()
+if __name__ == "__main__":
+    for i in range(1, 20):
+	    if is_prime(i + 1):
+            print(i + 1, end=" ")
+    print()
